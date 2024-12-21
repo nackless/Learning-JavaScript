77f1fd949d88293018ef5458681276074c184d3e
nackless <108402910+nackless@users.noreply.github.com>
2024-12-21 23:58:03 +0000

    Changed title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Learning JavaScript - My journey as I do it';
+export const SITE_DESCRIPTION = 'Welcome, I hope I can find some useful pearls for you!';
